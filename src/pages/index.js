829f2533a89484a20d9ab527e7d440512c59ee1e
ChRami <rami_chaaban@hotmail.com>
2020-11-09 20:32:02 -0500

    Added a Layout directory and configured code appearance

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,18 +5,35 @@ import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to my Website.</p>
-    <p>This will be my own personal website.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link> <br />
-    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
-  </Layout>
-)
+const IndexPage = () => {
+  return(
+    <Layout>
+      <div>
+        <h1>Hello World!</h1>
+        <p>Contact me:
+          Here is my github <a href="https://github.com/ChRami" target="_blank">GitHub</a>
+        </p>
+      </div>
+    </Layout>
+  )
+  // return(
+  //     <Layout>
+  //       <SEO title="Home" />
+  //       <h1>Hi people</h1>
+  //       <p>Welcome to my Website.</p>
+  //       <p>This will be my own personal website.</p>
+  //       <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+  //         <Image />
+  //       </div>
+  //       <Link to="/page-2/">Go to page 2</Link> <br />
+  //       <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
+  //     </Layout>
+  //   )
+
+}
+
+
+
+
 
 export default IndexPage
